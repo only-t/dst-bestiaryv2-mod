e7b79d5cd8e052558ac5920ac70c231f1c00ab51
-t- <97555422+only-t@users.noreply.github.com>
2022-07-16 18:48:51 +0200

    Initial commit.

---
+++ modinfo.lua
@@ -0,0 +1,43 @@
+name = "󰀈 Bestiary 󰀈"
+description = [[The Bestiary adds a new book to Don't Starve Together with all the information of the different creatues of The Constant!
+
+If you want to have all the information from the start disable Discoverable Mobs in the configurations!
+If you want to be able to open the bestiary without using the book disable Bestiary as an Item in the configurations!
+
+Current version: 1.1.0 󰀔
+- Killing bosses will now unlock all information about that boss for every nearby player
+- Added a configuration option to allow the bestiary to be opened using a book or a widget (left side of the inventory)
+- ONLY IF [API] Mods In Menu IS ENABLED: Added the Bestiary to the main manu (located in the Compendium)
+]]
+author = "-т-"
+version = "1.2.0"
+forumthread = "forums/topic/137812-the-bestiary-mod-full-release"
+icon_atlas = "icon.xml"
+icon = "icon.tex"
+client_only_mod = false
+all_clients_require_mod = true
+dst_compatible = true
+dont_starve_compatible = false
+priority = -99999999
+api_version = 10
+
+configuration_options = {
+    {
+        name = "Discoverable Mobs",
+        options = {
+            { description = "Yes", data = true },
+            { description = "No", data = false }
+        },
+
+        default = true,
+    },
+    {
+        name = "Bestiary as an Item",
+        options = {
+            { description = "Yes", data = true },
+            { description = "No", data = false }
+        },
+
+        default = true,
+    }
+}
---
+++ modmain.lua
@@ -0,0 +1,17 @@
+----						  [ Coder ]						  ----
+---- 							 -т-						  ----
+----  https://forums.kleientertainment.com/profile/365042-t/  ----
+----  					  Discord: -т-#1234				  	  ----
+
+local require = GLOBAL.require
+
+GLOBAL.CHEATS_ENABLED = true
+require("debugkeys")
+
+--\/ INIT \/--
+
+--/\ INIT /\--
+
+--\/ EXTERNAL CODE \/--
+
+--/\ EXTERNAL CODE /\--
