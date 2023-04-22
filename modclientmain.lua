28a8470c045948cf88b60cac551fb13586511175
-t- <onlyt.coding@gmail.com>
2023-04-23 00:57:42 +0200

    Added Mods in Menu support.

---
+++ modclientmain.lua
@@ -0,0 +1,16 @@
+env._G = GLOBAL._G
+GLOBAL.setfenv(1, env)
+
+--\/ INIT \/--
+
+modimport("init/init_assets")
+modimport("init/init_strings")
+modimport("init/init_tuning")
+
+--/\ INIT /\--
+
+--\/ EXTERNAL CODE \/--
+
+
+
+--/\ EXTERNAL CODE /\--
